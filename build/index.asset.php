3b7bb819bd9d0e5bfc50deadc1df7120b6b66601
Cory Hughart <cory@coryhughart.com>
2026-06-03 09:57:28 -0500

    include build files

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n', 'wp-token-list'), 'version' => '794812c43abba8be867e');
